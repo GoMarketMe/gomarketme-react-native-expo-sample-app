2b1f4da1a9b27cfc90a455e2862dba2aca897110
tpeinoit <toni.peinoit@gmail.com>
2024-10-27 17:59:44 +0100

    Installed the GoMarketMe SDK

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import GoMarketMe from 'gomarketme-react-native-expo';
 
 export default function App() {
+
+  const initializeSDK = async () => {
+    await GoMarketMe.initialize('YOUR_API_KEY'); // Replace with your actual API key
+  };
+
+  initializeSDK();
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hi, there!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,1 @@
+# gomarketme-react-native-expo-sample-app
